5f2834308b712bc75161e99d3bdf68ab9322e304
Sean Nguyen <seannguyen5696@gmail.com>
2018-11-06 01:16:00 +1100

    added relationship_1

--- app/Model/Like.php
+++ app/Model/Like.php
@@ -4,7 +4,18 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Model\Reply;
+use App\User;
+
 class Like extends Model
 {
     //
+    public function replies()
+    {
+      return $this->belongsTo(Reply::class);
+    }
+    public function user()
+    {
+      return $this->belongsTo(User::class);
+    }
 }
